54eec65ff9889dc9745431fae6db146bec645f42
VjacheslavZ <vjacheslav.zemlianiy@fluvius.co>
2020-11-05 16:54:21 +0200

    remove namespaces

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,6 @@
-/// <reference path='models/drag-drop.ts' />
-/// <reference path='models/project.ts' />
+import { ProjectInput } from "./components/project-input.js";
+import { ProjectList } from "./components/project-list.js";
 
-/// <reference path='state/project.ts' />
-/// <reference path='util/validation.ts' />
-/// <reference path='decorators/autobind.ts' />
-
-/// <reference path='components/base-component.ts' />
-/// <reference path='components/project-item.ts' />
-/// <reference path='components/project-list.ts' />
-/// <reference path='components/project-input.ts' />
-
-namespace App {
-  new ProjectInput();
-  new ProjectList('active');
-  new ProjectList('finished');
-}
+new ProjectInput();
+new ProjectList('active');
+new ProjectList('finished');
--- src/components/base-component.ts
+++ src/components/base-component.ts
@@ -1,38 +1,36 @@
-namespace App {
-  export abstract class Component<T extends HTMLElement, U extends HTMLElement> {
-    templateElement: HTMLTemplateElement;
-    hostElement: T;
+export abstract class Component<T extends HTMLElement, U extends HTMLElement> {
+  templateElement: HTMLTemplateElement;
+  hostElement: T;
 
-    constructor(
-      templateId: string,
-      hostElementId: string,
-      insertAtStart: boolean,
-      newElementId?: string,
-    ) {
-      this.templateElement = document.getElementById(templateId)! as HTMLTemplateElement;
-      this.hostElement = document.getElementById(hostElementId)! as T;
+  constructor(
+    templateId: string,
+    hostElementId: string,
+    insertAtStart: boolean,
+    newElementId?: string,
+  ) {
+    this.templateElement = document.getElementById(templateId)! as HTMLTemplateElement;
+    this.hostElement = document.getElementById(hostElementId)! as T;
 
-      const importedNode = document.importNode(this.templateElement.content, true)
-      this.element = importedNode.firstElementChild as U;
+    const importedNode = document.importNode(this.templateElement.content, true)
+    this.element = importedNode.firstElementChild as U;
 
-      if (newElementId) {
-        this.element.id = newElementId;
-      }
-
-      this.attach(insertAtStart);
+    if (newElementId) {
+      this.element.id = newElementId;
     }
 
-    element: U;
-
-    private attach(insertAtBeggining: boolean) {
-      this.hostElement.insertAdjacentElement(
-        insertAtBeggining ? 'afterbegin' : 'beforeend',
-        this.element
-      );
-    }
-
-    abstract configure(): void;
-    abstract renderContent(): void;
+    this.attach(insertAtStart);
   }
+
+  element: U;
+
+  private attach(insertAtBeggining: boolean) {
+    this.hostElement.insertAdjacentElement(
+      insertAtBeggining ? 'afterbegin' : 'beforeend',
+      this.element
+    );
+  }
+
+  abstract configure(): void;
+  abstract renderContent(): void;
 }
 
